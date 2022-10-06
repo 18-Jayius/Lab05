5457ad407b1b0dcd9d579ab17aa062f49897aaba
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-06 13:05:05 +0800

    added audio and created particle system as well as restart script

--- Assets/CollisionScript.cs
+++ Assets/CollisionScript.cs
@@ -8,7 +8,8 @@ public class CollisionScript : MonoBehaviour
 {
     public Text scoretxt;
     public int score;
-
+    public GameObject particlesys;
+    private GameObject test;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +19,21 @@ public class CollisionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameObject.FindGameObjectsWithTag("Coin").Length == 0)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
     }
 
     public void OnTriggerEnter (Collider collision)
     {
         if(collision.gameObject.tag == "Coin")
         {
+            test = Instantiate(particlesys, transform.position, Quaternion.identity);
             score = score + 10;
             scoretxt.text = "Score : " + score;
             Destroy(collision.gameObject);
+            Destroy(test, 2f);
 
         }
         if (collision.gameObject.tag == "Water")
--- Assets/TimerScript.cs
+++ Assets/TimerScript.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class TimerScript : MonoBehaviour
 {
+    public float timer = 60;
+    public Text timertxt;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class TimerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer = Mathf.Round(timer * 100f) / 100f;
+        timer = timer - Time.deltaTime;
+        timertxt.text = "Timer : " + timer;
+
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
     }
 }
