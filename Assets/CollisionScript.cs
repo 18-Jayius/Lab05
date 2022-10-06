cdbf11cdf6e4c7bf7e557d117652195847ef908b
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-06 12:25:31 +0800

    created timer script and made collision/trigger script

---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionScript : MonoBehaviour
+{
+    public Text scoretxt;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter (Collider collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            score = score + 10;
+            scoretxt.text = "Score : " + score;
+            Destroy(collision.gameObject);
+
+        }
+        if (collision.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+
+    }
+}
